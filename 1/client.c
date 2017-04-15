9847fe76e576601f805ac15e0759ded3b01f7219
Avinash Sonawane <rootkea@gmail.com>
2017-04-15 12:31:06 +0530

    Add headers to suppress warnings

--- 1/client.c
+++ 1/client.c
@@ -10,6 +10,9 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 
--- 1/server.c
+++ 1/server.c
@@ -13,6 +13,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
+#include <unistd.h>
 
 void error(char *msg)
 {
